76498ef0b2a6ca6513182fe045b54b8fed0a7b6a
TahminaChowdhury <tahmina10st@gmail.com>
2022-11-28 12:09:57 +0600

    commit

--- src/Pages/Home/Blog/Blog.js
+++ src/Pages/Home/Blog/Blog.js
@@ -2,6 +2,8 @@ import { Box, Grid } from '@material-ui/core';
 import React from 'react';
 import './Blog.css';
 import img1 from '../../../images/portrait-smiling-beautiful-girl-her-handsome-boyfriend-casual-summer-clothes-woman-with-bottle-water-straw_158538-5358.jpg';
+import img2 from '../../../images/Models-wear-HUGO-HG-SUN-RX-01-and-HG-SUN-RX-02-available-at-Specsavers-removebg-preview.png';
+import img3 from '../../../images/160321_image-guide_LOOKBOOK_image_39.jpg';
 
 const Blog = () => {
   return (
@@ -9,17 +11,16 @@ const Blog = () => {
       sx={{
         width: '100%',
       }}
-      style={{margin: '100px 0px'}}
+      style={{ margin: '100px 0px' }}
     >
-      <h1 className='text-center mb-5 pb-5'>Our Blogs</h1>
+      <h1 className="text-center mb-5 pb-5">Our Blogs</h1>
       <Grid container spacing={2}>
-        
         <Grid
           item
           xs={12}
           sm={12}
           md={4}
-          style={{ display: 'flex', justifyContent: 'center'}}
+          style={{ display: 'flex', justifyContent: 'center' }}
         >
           <div className="blog-container">
             <div className="blog-img-div">
@@ -30,51 +31,63 @@ const Blog = () => {
               </div>
             </div>
             <div className="blog-content">
-              <h3 className="blog-title">Lorem ipsum dolor</h3>
+              <h3 className="blog-title">GORDON-GLASSES</h3>
               <p className="description">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Deleniti iusto dignissimos consequatur eligendi excepturi esse
-                sequi...
+                One morning, when Gregor Samsa woke from troubled dreams, he
+                found himself transformed in his bed into a horrible vermin. He
+                lay on his armour-like...
               </p>
               <button className="read-more">Read more</button>
             </div>
           </div>
         </Grid>
-        <Grid item xs={12} sm={12} md={4} style={{ display: 'flex', justifyContent: 'center'}}>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={4}
+          style={{ display: 'flex', justifyContent: 'center' }}
+        >
           <div className="blog-container">
             <div className="blog-img-div">
-              <img src={img1} alt="" />
+              <img src={img2} alt="" />
               <div className="date">
                 <span className="day">26</span>
                 <span className="month">June</span>
               </div>
             </div>
             <div className="blog-content">
-              <h3 className="blog-title">Lorem ipsum dolor</h3>
+              <h3 className="blog-title">GORDON-GLASSE</h3>
               <p className="description">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Deleniti iusto dignissimos consequatur eligendi excepturi esse
-                sequi...
+                One morning, when Gregor Samsa woke from troubled dreams, he
+                found himself transformed in his bed into a horrible vermin. He
+                lay on his armour-like...
               </p>
               <button className="read-more">Read more</button>
             </div>
           </div>
         </Grid>
-        <Grid item xs={12} sm={12} md={4} style={{ display: 'flex', justifyContent: 'center' }}>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={4}
+          style={{ display: 'flex', justifyContent: 'center' }}
+        >
           <div className="blog-container">
             <div className="blog-img-div">
-              <img src={img1} alt="" />
+              <img src={img3} alt="" />
               <div className="date">
                 <span className="day">26</span>
                 <span className="month">June</span>
               </div>
             </div>
             <div className="blog-content">
-              <h3 className="blog-title">Lorem ipsum dolor</h3>
+              <h3 className="blog-title">GORDON-GLASSE</h3>
               <p className="description">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Deleniti iusto dignissimos consequatur eligendi excepturi esse
-                sequi...
+                One morning, when Gregor Samsa woke from troubled dreams, he
+                found himself transformed in his bed into a horrible vermin. He
+                lay on his armour-like...
               </p>
               <button className="read-more">Read more</button>
             </div>
